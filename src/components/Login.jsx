44a521c2723dcfc2b227015a22ab3e6ffa1720fe
Steven Colburn <stevencolburn@xamdes.com>
2018-09-28 14:26:27 -0700

    username and password for login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,10 +7,37 @@
 import styled from 'styled-components';
 // import { Link, Switch, Route } from 'react-router-dom';
 
-function Login(){
+function Login()
+{
+  let _username, _password;
+  _username = _password = null;
+  function handleNewTicketFormSubmission(event)
+  {
+    event.preventDefault();
+    // props.onNewTicketCreation({username: _username.value, password: _password.value});
+    console.log(_username.value);
+    console.log(_password.value);
+    _username.value = '';
+    _password.value = '';
+}
   return (
     <Main>
       <h1>User Login</h1>
+        <form className="" onSubmit={handleNewTicketFormSubmission}>
+          <input
+            className=""
+            type='text'
+            id='username'
+            placeholder='username'
+            ref={(input) => {_username = input;}}/>
+          <input
+            className=""
+            type='text'
+            id='password'
+            placeholder='Password'
+            ref={(input) => {_password = input;}}/>
+          <button className="btn btn-submit" type='submit'>Login!</button>
+  </form>
     </Main>
   );
 }
